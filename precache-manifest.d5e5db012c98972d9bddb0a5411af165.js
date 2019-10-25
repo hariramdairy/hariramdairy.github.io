0221d1387482b388067767a21fcbc6722f9e72ac
mhtkmr <mhtkmr303@gmail.com>
2019-10-25 14:14:24 +0530

    init commit

---
+++ precache-manifest.d5e5db012c98972d9bddb0a5411af165.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da73408adb5706df560a8ff6230afcce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25b39f47fd17625b887f",
+    "url": "/static/css/2.9db7d3cb.chunk.css"
+  },
+  {
+    "revision": "e7b29cfe1bcaccb0fc37",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "25b39f47fd17625b887f",
+    "url": "/static/js/2.19ca690b.chunk.js"
+  },
+  {
+    "revision": "e7b29cfe1bcaccb0fc37",
+    "url": "/static/js/main.597d013b.chunk.js"
+  },
+  {
+    "revision": "174be94e5e823043f5e6",
+    "url": "/static/js/runtime~main.2033e80b.js"
+  },
+  {
+    "revision": "a9d99c5edd1d246aff15221bc76dccd6",
+    "url": "/static/media/fancybox1.a9d99c5e.jpeg"
+  },
+  {
+    "revision": "380a8b42917382b256f86795e7bac8bb",
+    "url": "/static/media/fancybox2.380a8b42.jpeg"
+  },
+  {
+    "revision": "ee9e38a61e87d9ed6e5c6ad1b0b6c869",
+    "url": "/static/media/hariram-logo.ee9e38a6.png"
+  },
+  {
+    "revision": "e4028acfaeb50032ba960a2e840caa95",
+    "url": "/static/media/kajukatli.e4028acf.jpeg"
+  },
+  {
+    "revision": "6b538c20e66ced8946a62ceda9152b4b",
+    "url": "/static/media/slider-cookie.6b538c20.jpg"
+  },
+  {
+    "revision": "6c1cd328e1017aa8de8ef7661b582f41",
+    "url": "/static/media/slider-cow.6c1cd328.jpg"
+  },
+  {
+    "revision": "07be6f63ce670f3ad965ec6c72755e51",
+    "url": "/static/media/slider-sweets.07be6f63.jpg"
+  }
+]);
